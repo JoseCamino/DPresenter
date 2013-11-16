537b757a83a5f9434f70f0fc8a1a83ebece50987
JoseCamino <jcamino86@gmail.com>
2013-11-16 18:16:16 -0500

    Made corrections to the comments

--- Parser/ParserFacade.py
+++ Parser/ParserFacade.py
@@ -6,7 +6,7 @@ class ParserFacade:
    @staticmethod
    def mergeSlides(slides):
       'Merges any number of slides into a deck. Input is the slides binary data. '
-      'Returns the path of the a temporary deck that can be deleted after it is downloaded.'
+      'Returns the path of a temporary deck that can be deleted after it is downloaded.'
       return Parser.mergeSlides(slides)
 
    @staticmethod
@@ -17,13 +17,13 @@ class ParserFacade:
    @staticmethod
    def generateImageFromFile(file_path, output_dir):
       'Generates an image file from each slide. Returns the paths to the images as '
-      '[.../Image1.pptx, .../Image2.pptx, ...].'
+      '[.../Slide1.JPG, .../Slide2.JPG, ...].'
       return Parser.generateImageFromFile(file_path, output_dir)
 
    @staticmethod
    def generateImageFromData(data, output_dir):
       'Generates an image file from each slide. Input is the binary data of the slide or deck.'
-      'Returns the paths to the images as [.../Image1.pptx, .../Image2.pptx, ...].'
+      'Returns the paths to the images as [.../Slide1.JPG, .../Slide2.JPG, ...].'
       return Parser.generateImageFromData(data, output_dir)  
 
 
